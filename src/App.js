a4f198a1917f82db334c16311554f6812267ac99
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-06-04 22:03:14 +0600

    update the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './components/actions';
 function App() {
+  //follow the reducer index file
+  const counter = useSelector((state) => state.counter);
+  const islogged = useSelector((state) => state.islogged);
+  const dispatch = useDispatch();
   return (
     <div className='App'>
-      <h2>Hello World</h2>
+      <h2>Counter {counter}</h2>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {islogged ? <h3>yes i see</h3> : ''}
     </div>
   );
 }
--- src/components/reducers/counter.js
+++ src/components/reducers/counter.js
@@ -3,7 +3,7 @@ const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return state + 1;
-    case 'INCREMENT':
+    case 'DECREMENT':
       return state - 1;
     default:
       return state;
